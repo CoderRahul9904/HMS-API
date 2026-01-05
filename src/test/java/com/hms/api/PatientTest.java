d4ba18aff7f2add9d850aeb36f3383edc682de88
Rahul <mouryarahul543@gmail.com>
2026-01-05 20:26:38 +0530

    junit test for tranctional annotation

--- src/test/java/com/hms/api/PatientTest.java
+++ src/test/java/com/hms/api/PatientTest.java
@@ -2,6 +2,7 @@ package com.hms.api;
 
 import com.hms.api.entities.Patient;
 import com.hms.api.repository.PatientRepository;
+import com.hms.api.services.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +13,20 @@ import java.util.List;
 public class PatientTest {
 
     @Autowired
+    private PatientService patientService;
+    @Autowired
     private PatientRepository patientRepository;
     @Test
     public void testPatientRepository(){
         List<Patient> patients = patientRepository.findAll();
         for (Patient p : patients) {
-            System.out.println("Patient: " + p.getId() + " - " + p.getFirstName() + " " + p.getLastName());
+//            System.out.println("Patient: " + p.getId() + " - " + p.getFirstName() + " " + p.getLastName());
         }
+    }
+
+    @Test
+    public void testPatientRepository2(){
+        Patient p = patientService.getPatientById(1L);
+        System.out.println(p);
     }
 }
