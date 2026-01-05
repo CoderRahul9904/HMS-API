b9e680eb5b9e479b10e6de6b88bc8dd83a971701
Rahul <mouryarahul543@gmail.com>
2026-01-05 21:08:33 +0530

    updating

--- src/main/java/com/hms/api/entities/Patient.java
+++ src/main/java/com/hms/api/entities/Patient.java
@@ -1,5 +1,6 @@
 package com.hms.api.entities;
 
+import com.hms.api.entities.type.BloodGroupType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -41,6 +42,9 @@ public class Patient {
 
     @Column(unique = true)
     private String nationalId;
+
+    @Enumerated(EnumType.STRING)
+    private BloodGroupType bloodGroupType;
 
     private String emergencyContactName;
 
