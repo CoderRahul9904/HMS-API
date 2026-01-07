7e6f4630cf323482d944c391cf665c0c912e859a
Rahul <mouryarahul543@gmail.com>
2026-01-07 15:21:29 +0530

    customUserDetails fetch from db

---
+++ src/main/java/com/hms/api/security/CustomUserDetailService.java
@@ -0,0 +1,21 @@
+package com.hms.api.security;
+
+import com.hms.api.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByUsername(username).orElseThrow();
+    }
+}
