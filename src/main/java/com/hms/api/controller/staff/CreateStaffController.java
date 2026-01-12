bb1fbd454d1a00821148ec038926256721e9ab67
Rahul <mouryarahul543@gmail.com>
2026-01-12 23:37:30 +0530

    CreateStaffController

---
+++ src/main/java/com/hms/api/controller/staff/CreateStaffController.java
@@ -0,0 +1,21 @@
+package com.hms.api.controller.staff;
+
+import com.hms.api.dto.staff.CreateStaffDto;
+import com.hms.api.services.staff.Impl.StaffService;
+import com.hms.api.services.staff.StaffImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class CreateStaffController {
+
+    private final StaffService staffService;
+    @PostMapping("/users/create/{userId}")
+    public ResponseEntity<CreateStaffDto> registerStaffInfo(@PathVariable String userId, @RequestBody CreateStaffDto createStaffDto) {
+        return ResponseEntity.ok(staffService.createStaff(userId,createStaffDto));
+    }
+}
