695c9063098b1e039a4e56ba8e4dee2f810ae96c
Rahul <mouryarahul543@gmail.com>
2026-01-12 23:39:01 +0530

    StaffService services

--- src/main/java/com/hms/api/services/staff/Impl/StaffService.java
+++ src/main/java/com/hms/api/services/staff/Impl/StaffService.java
@@ -3,15 +3,16 @@ package com.hms.api.services.staff.Impl;
 import com.hms.api.dto.staff.CreateStaffDto;
 import com.hms.api.dto.staff.LoginStaffRequestDto;
 import com.hms.api.dto.staff.LoginStaffResponseDto;
-import com.hms.api.dto.user.CreateUserDto;
-import com.hms.api.dto.user.UserResponseDto;
 import com.hms.api.entity.Staff;
 import com.hms.api.entity.User;
 import com.hms.api.repository.StaffRepository;
 import com.hms.api.services.staff.StaffImpl;
+import com.hms.api.util.GenerateTokenUtil;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class StaffService implements StaffImpl {
 
     private AuthenticationManager authenticationManager;
+    private GenerateTokenUtil generateTokenUtil;
     private final StaffRepository staffRepository;
     private final ModelMapper modelMapper;
 
@@ -41,11 +43,16 @@ public class StaffService implements StaffImpl {
 
 
     @Override
-    public LoginStaffResponseDto loginStaff(Long staffId) {
+    public LoginStaffResponseDto loginStaff(String staffCode, LoginStaffRequestDto dto) {
+        if(staffRepository.findByStaffCode(staffCode).isEmpty()) throw new IllegalArgumentException("Staff not found");
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword())
+        );
+        User user=(User) authentication.getPrincipal();
 
-
+        String token=generateTokenUtil.generateJwtAccessToken(user);
+        return new LoginStaffResponseDto(token, user.getId());
     }
-
 
 
 
