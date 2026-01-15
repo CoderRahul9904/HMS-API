5bafdc3e7d543bbbd9009b19c4bcaddef84a350b
Rahul <mouryarahul543@gmail.com>
2026-01-15 20:48:18 +0530

    UserController

--- src/main/java/com/hms/api/controller/user/UserController.java
+++ src/main/java/com/hms/api/controller/user/UserController.java
@@ -4,14 +4,14 @@ package com.hms.api.controller.user;
 import com.hms.api.dto.user.CreateUserDto;
 import com.hms.api.dto.user.UserResponseDto;
 import com.hms.api.services.user.Impl.UserService;
-import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/auth")
 public class UserController {
 
     private final UserService userService;
@@ -20,7 +20,9 @@ public class UserController {
         this.userService = userService;
     }
     @PostMapping("/create/user")
-    public UserResponseDto registerUser(CreateUserDto createUserDto) {
+    public UserResponseDto registerUser(@RequestBody CreateUserDto createUserDto) {
         return userService.createUser(createUserDto);
     }
+
+
 }
