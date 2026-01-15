3a2219204dbd84e239c5bdbbf49d9df5c8b15db9
Rahul <mouryarahul543@gmail.com>
2026-01-15 20:49:02 +0530

    LoginStaffResponseDto

--- src/main/java/com/hms/api/dto/staff/LoginStaffResponseDto.java
+++ src/main/java/com/hms/api/dto/staff/LoginStaffResponseDto.java
@@ -3,16 +3,11 @@ package com.hms.api.dto.staff;
 import lombok.*;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class LoginStaffResponseDto {
     private String token;
     private String staffCode;
-
-    public LoginStaffResponseDto(String token, Long id) {
-        this.token = token;
-        this.staffCode = String.valueOf(id);
-    }
 }
