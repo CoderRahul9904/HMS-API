6a3671850085d54db8370c311b2ce9fc737fe42b
Rahul <mouryarahul543@gmail.com>
2026-01-18 21:11:40 +0530

    LoginStaffController

---
+++ src/main/java/com/hms/api/controller/staff/LoginStaffController.java
@@ -0,0 +1,22 @@
+package com.hms.api.controller.staff;
+
+import com.hms.api.dto.staff.LoginStaffRequestDto;
+import com.hms.api.dto.staff.LoginStaffResponseDto;
+import com.hms.api.services.staff.Impl.StaffService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class LoginStaffController {
+
+    private final StaffService staffService;
+
+    @PostMapping("/users/login")
+    public ResponseEntity<LoginStaffResponseDto> loginStaff(@RequestBody LoginStaffRequestDto loginStaffRequestDto) {
+        System.out.println("LOGIN CONTROLLER HIT");
+        return ResponseEntity.ok(staffService.loginStaff(loginStaffRequestDto));
+    }
+}
