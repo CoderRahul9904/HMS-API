2745c0a342ebeba82a0099d39955dcb8a9ab24ba
Rahul <mouryarahul543@gmail.com>
2026-01-18 21:14:07 +0530

    StaffImpl interface

--- src/main/java/com/hms/api/services/staff/StaffImpl.java
+++ src/main/java/com/hms/api/services/staff/StaffImpl.java
@@ -6,9 +6,10 @@ import com.hms.api.dto.staff.LoginStaffResponseDto;
 import com.hms.api.dto.user.CreateUserDto;
 import com.hms.api.dto.user.UserResponseDto;
 import com.hms.api.entity.Staff;
+import com.hms.api.entity.User;
 
 public interface StaffImpl {
-    CreateStaffDto createStaff(String id,CreateStaffDto createStaffDto);
+    CreateStaffDto createStaff(User user, CreateStaffDto createStaffDto);
     LoginStaffResponseDto loginStaff(LoginStaffRequestDto dto);
 
 }
