e0960b3673d9cd40898d5395633250bb45f51b7b
Rahul <mouryarahul543@gmail.com>
2026-03-06 21:59:50 +0530

    insurance schema

--- src/main/java/com/hms/api/entity/Insurance.java
+++ src/main/java/com/hms/api/entity/Insurance.java
@@ -1,10 +1,34 @@
 package com.hms.api.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(
+        name = "insurance",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "policyNumber")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Insurance {
+
     @Id
-    public long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String providerName;
+
+    @Column(nullable = false, length = 50)
+    private String policyNumber;
+
+    @Column(nullable = false)
+    private Double coverageAmount;
+
+    private String expiryDate;
 }
