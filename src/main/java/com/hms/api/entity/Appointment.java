c6b6ac13c19fa69232003ef8a194dfe162399d49
Rahul <mouryarahul543@gmail.com>
2026-03-18 20:29:17 +0530

    update in appointment

--- src/main/java/com/hms/api/entity/Appointment.java
+++ src/main/java/com/hms/api/entity/Appointment.java
@@ -1,23 +1,32 @@
 package com.hms.api.entity;
+
 import com.hms.api.entity.type.AppointmentStatus;
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "appointments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Appointment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "patient_id")
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
     private Patient patient;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "doctor_id")
-    private Staff doctor;
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
 
     private LocalDateTime appointmentTime;
 
@@ -25,4 +34,9 @@ public class Appointment {
     private AppointmentStatus status;
 
     private LocalDateTime createdAt;
+
+    @PrePersist
+    void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
